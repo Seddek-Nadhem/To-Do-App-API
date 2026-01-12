591221fceb7ba97ac24cf9d00b09cc59bdbdee69
Seddek Nadhem <seddeknadhem@gmail.com>
2026-01-12 11:52:21 +0300

    fixed a typo + applied type hinting

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
@@ -10,12 +11,12 @@ use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index() : AnonymousResourceCollection
     {
-        return TaskResource::collection(Auth::user()->tasks()->latest(0)->paginate(5));
+        return TaskResource::collection(Auth::user()->tasks()->latest()->paginate(5));
     }
 
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request) : TaskResource
     {
         $validated = $request->validated();
         
@@ -24,14 +25,14 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
-    public function show($id)
+    public function show($id): TaskResource
     {
         $task = Auth::user()->tasks()->findOrFail($id);    
 
         return new TaskResource($task);
     }
 
-    public function update(UpdateTaskRequest $request, $id)
+    public function update(UpdateTaskRequest $request, $id): TaskResource
     {
         $task = Auth::user()->tasks()->findOrFail($id);
         
@@ -40,7 +41,7 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
-    public function destroy($id)
+    public function destroy($id): Response
     {
         $task = Auth::user()->tasks()->findOrFail($id);
         
